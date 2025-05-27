29562de6dbfaca4b1f1b6b71b8b80b9b00d69afd
Miraslau <forse2408@gmail.com>
2025-05-27 18:11:54 +0200

    delete unessery logic

--- app/ui/client-profile/client-profile.tsx
+++ app/ui/client-profile/client-profile.tsx
@@ -1,61 +1,29 @@
-"use client";
-
 import React from "react";
-import Button from "@/app/ui/button/button";
 import UserDefaultImage from "@/app/assets/images/user.png";
-import UploadImage from "@/app/ui/upload-image/upload-Image";
-import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/lib/auth";
 
-const ClientProfile = () => {
-  const { update, data: session } = useSession();
-  const uploadStagedFile = async (stagedFile: File | Blob) => {
-    console.log("staged file: ", stagedFile);
-    const form = new FormData();
-    form.set("file", stagedFile);
-    form.append("folderRoot", "Users");
-    form.append("folderName", `user-${session?.user?.username}`);
-
-    const response = await fetch("/api/upload", {
-      method: "POST",
-      body: form,
-    });
-
-    const data = await response.json();
-
-    await update({
-      image: data.imgUrl,
-    });
-
-    await fetch("/api/user", {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        id: session?.user.id,
-        url: data.imgUrl,
-      }),
-    });
-
-    console.log("image url: ", data.imgUrl);
-  };
+const ClientProfile = async () => {
+  const session = await getServerSession(authOptions);
 
   return (
     <div className="p-[32px]">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-[24px]">
-          <UploadImage
+          <Image
+            className="rounded-[50%]"
             src={
               session && session.user.image
                 ? session.user.image
                 : UserDefaultImage
             }
-            alt="User"
-            priority
+            alt="user"
+            priority={true}
             width={100}
             height={100}
-            handleSubmit={uploadStagedFile}
           />
+
           <div>
             <div className="text-secondary-500 text-[20px] font-medium">
               {session ? session.user.username : ""}
@@ -65,11 +33,7 @@ const ClientProfile = () => {
             </div>
           </div>
         </div>
-        <Button size="medium" customClassName="pl-[32px] pr-[32px]">
-          Edit
-        </Button>
       </div>
-      <div>form inputs</div>
     </div>
   );
 };
